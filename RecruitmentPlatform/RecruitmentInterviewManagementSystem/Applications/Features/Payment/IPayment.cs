267a933aa53d64d1e423b0e4e10c68e8d94c3d1f
kdadeptrai <funnymobile1230@gmail.com>
2026-03-13 16:40:29 +0700

    Update: delete foreign key from order to employerProfile, candidateID

--- RecruitmentPlatform/RecruitmentInterviewManagementSystem/Applications/Features/Payment/IPayment.cs
+++
@@ -1,9 +0,0 @@
-﻿using RecruitmentInterviewManagementSystem.Applications.Features.Payment.DOT;
-
-namespace RecruitmentInterviewManagementSystem.Applications.Features.Payment
-{
-    public interface IPayment
-    {
-        Task<ResponsePayment> Execute(CreatePaymentDTO request);
-    }
-}
